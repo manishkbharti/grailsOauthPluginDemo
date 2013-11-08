f67f761f19c60d20a7e33367768a08a4137553bd
Manish Bharti <manish.bharti1988@gmail.com>
2013-11-09 01:47:35 +0530

    Delete Extra files

--- target/work/plugins/oauth-2.1.0/grails-app/conf/OauthUrlMappings.groovy
+++
@@ -1,8 +0,0 @@
-class OauthUrlMappings {
-
-    static mappings = {
-
-        "/oauth/$provider/callback"(controller: 'oauth', action: 'callback')
-        "/oauth/$provider/authenticate"(controller: 'oauth', action: 'authenticate')
-    }
-}
