35ff26f5f14398f5c29fda420d3af1fa16b455a0
Manish Bharti <manish.bharti1988@gmail.com>
2013-11-23 22:39:34 +0530

    port changed to 8080

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -5,7 +5,6 @@ grails.project.test.reports.dir = "target/test-reports"
 grails.project.work.dir = "target/work"
 grails.project.target.level = 1.6
 grails.project.source.level = 1.6
-grails.server.port.http = 8181
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
 grails.project.fork = [
